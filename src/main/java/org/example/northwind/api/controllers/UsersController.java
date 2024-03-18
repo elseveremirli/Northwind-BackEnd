cdd7172dbf0687bb0be238859fed308e41144a79
Elsever-Emirli <elseveremirli10@gmail.com>
2024-03-18 23:39:34 +0400

    validation errors handler

--- src/main/java/org/example/northwind/api/controllers/UsersController.java
+++ src/main/java/org/example/northwind/api/controllers/UsersController.java
@@ -1,17 +1,18 @@
 package org.example.northwind.api.controllers;
 
+import jakarta.validation.Valid;
 import org.example.northwind.business.abstracts.UserService;
 import org.example.northwind.core.entities.User;
-import org.example.northwind.core.utilities.results.DataResult;
-import org.example.northwind.core.utilities.results.Result;
-import org.example.northwind.core.utilities.results.SuccessDataResult;
-import org.example.northwind.core.utilities.results.SuccessResult;
+import org.example.northwind.core.utilities.results.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users/")
@@ -23,11 +24,23 @@ public class UsersController {
     }
 
     @PostMapping("add")
-    public ResponseEntity<?> add(@RequestBody User user) {
+    public ResponseEntity<?> add(@Valid @RequestBody User user) {
         return ResponseEntity.ok(userService.add(user));
     }
 
-    public DataResult<User> findByEmail(String email) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValditionException(MethodArgumentNotValidException exceptions){
+        Map<String,String> validationErrors = new HashMap<String,String>();
+        for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()){
+            validationErrors.put(fieldError.getField(),fieldError.getDefaultMessage());
+        }
+        ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors,"Dogrulama Hatalari");
+        return errors;
+    }
+
+    @GetMapping("findByEmail")
+    public DataResult<User> findByEmail(@RequestParam String email) {
         return new SuccessDataResult<User>(userService.findByEmail(email),"Result");
     }
 }
